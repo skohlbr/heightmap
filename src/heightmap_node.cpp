c93b8b47ea12c05f03905f1739911cee70521267
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2015-04-24 15:13:52 +0000

    Fix + query handling w/ transforms

--- src/heightmap_node.cpp
+++ src/heightmap_node.cpp
@@ -2,12 +2,21 @@
 #include <cstdio>
 #include <iostream>
 #include <mutex>
+#include <memory>
+
 #include <matrixstore.h>
 #include <ros/console.h>
 #include <ros/ros.h>
+#include "heightmap/Query.h"
 #include <sensor_msgs/PointCloud.h>
 #include <geometry_msgs/Point.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <tf/transform_listener.h>
+
+template<typename T, typename ... Args>
+std::unique_ptr<T> make_unique(Args&& ...cons_args) {
+	return std::unique_ptr<T>(new T(cons_args...));
+}
 
 using namespace visualization_msgs;
 
@@ -15,32 +24,17 @@ static const float CELL_SIZE_X = 0.1f;
 static const float CELL_SIZE_Y = 0.1f;
 
 msSparseMatrix_t *sm;
+std::string map_frame;
+
+std::unique_ptr<tf::TransformListener> tf_listener;
+
 ros::Publisher publisher;
 std::mutex pub_mutex;
 ros::Timer pub_timer;
-static const ros::Duration timerDuration {1.0}; // in seconds
-
 int row=0, col=0, num_rows=100, num_cols=100;
 double *buf;
 
-void handleInputMessage(const sensor_msgs::PointCloud& msg)
-{
-    for(const auto& point : msg.points) {
-        int x = point.x / CELL_SIZE_X;
-        int y = point.y / CELL_SIZE_Y;
-        double height = point.z;
-
-        double old_height;
-        msSparseMatrixRead(sm, &old_height, x, y, 1, 1);
-
-        if (isnan(old_height) || old_height < height)
-            msSparseMatrixWrite(sm, &height, 1, 1, x, y);
-    }
-
-    pub_timer.start();
-}
-    
-void publish()
+void publishVisualization()
 {
 	if (!pub_mutex.try_lock())
 		return;
@@ -54,7 +48,7 @@ void publish()
     }
 
 	sensor_msgs::PointCloud pointcloud;
-	pointcloud.header.frame_id = "map";
+	pointcloud.header.frame_id = map_frame;
 	
 	for(int i=0; i < num_rows; i++) {
 		for(int j=0; j < num_cols; j++) {
@@ -66,14 +60,78 @@ void publish()
 			point.z = buf[index];
 			pointcloud.points.push_back(point);
 
-			printf("%8.5f %8.5f %8.5f\n",
-				   point.x, point.y, point.z);
+			// printf("%8.5f %8.5f %8.5f\n",
+			// 	   point.x, point.y, point.z);
 		}
 	}
 
 	publisher.publish(pointcloud);
 	
 	pub_mutex.unlock();
+}
+
+void handleInputMessage(const sensor_msgs::PointCloud& msg)
+{
+	const sensor_msgs::PointCloud *msgp = &msg;
+	bool transformed = false;
+	
+	if (msg.header.frame_id != map_frame) {
+		transformed = true;
+		auto *new_msg = new sensor_msgs::PointCloud();
+		tf_listener->transformPointCloud(map_frame, msg, *new_msg);
+		msgp = new_msg;
+	}
+	
+    for(const auto& point : msgp->points) {
+        int x = point.x / CELL_SIZE_X;
+        int y = point.y / CELL_SIZE_Y;
+        double height = point.z;
+
+        double old_height;
+        msSparseMatrixRead(sm, &old_height, x, y, 1, 1);
+
+        if (isnan(old_height) || old_height < height)
+            msSparseMatrixWrite(sm, &height, 1, 1, x, y);
+    }
+
+	if (transformed)
+		delete msgp;
+	
+	publishVisualization();
+}
+
+bool handleQuery(heightmap::Query::Request &req,
+				 heightmap::Query::Response &res)
+{
+	ROS_DEBUG("serving request");
+	
+	res.samples = std::move(req.samplePoints);
+
+	for(auto& sample : res.samples) {
+	}
+
+	std::string cur_frame_id;
+	for(auto& sample : res.samples) {
+		if (sample.header.frame_id != map_frame) {
+			cur_frame_id = sample.header.frame_id;
+			tf_listener->transformPoint(map_frame, sample, sample);
+		}
+		
+		int x = sample.point.x / CELL_SIZE_X;
+		int y = sample.point.x / CELL_SIZE_Y;
+		double double_val;
+
+		msSparseMatrixRead(sm, &double_val, x, y, 1, 1);
+		
+		sample.point.z = double_val; // double -> float conversion here
+
+		if (!cur_frame_id.empty()) {
+			tf_listener->transformPoint(cur_frame_id, sample, sample);
+			cur_frame_id.clear();
+		}
+	}
+
+	return true;
 }
 
 int main(int argc, char **argv)
@@ -93,14 +151,15 @@ int main(int argc, char **argv)
 		buf[i] = 0.0f;
 	
     ros::NodeHandle nh;
+
+	tf_listener = make_unique<tf::TransformListener>(nh);
+	map_frame = "map";
+	
     ros::Subscriber sub = nh.subscribe("pointcloud", 256, handleInputMessage);
     publisher = nh.advertise<sensor_msgs::PointCloud>("heightmap_vis", 1, true);
 
-    pub_timer = nh.createTimer(timerDuration,
-                               [](const ros::TimerEvent& event) {
-                                   publish();
-                               });
-                                   
+	ros::ServiceServer service = nh.advertiseService("heightmap_query", handleQuery);
+
     ros::spin();
     return 0;
 }
